ba99e5d388a57fe05469be50ff51918252a08d2f
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-07-07 13:48:09 -0300

    fazendo o back da pagina de registro

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const express = require('express');
 const app = express();
 const path = require("path")
 const PORT = 2121;
-
+const section = require('express-session')
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     
-    const indexPath = path.join(__dirname, 'views', 'index.html')
+    const indexPath = path.join(__dirname, 'views', 'signUp.html')
     res.sendFile(indexPath)
 })
 app.listen(PORT,()=>{
 console.log(`Server Running at the Port ${PORT}`)
 })
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'd21193809',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.post('/signUp', (req,res)=>{
+    const {name, email, keypass} = req.body
+
+    req.session.user = {name, email, keypass};
+
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res) => {
+    const loginPath = path.join(__dirname, 'views', 'login.html');
+    res.sendFile(loginPath);
+  });
